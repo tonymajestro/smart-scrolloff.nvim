d9260cf9a41adfc30b3dbeed1599d89ba9ae1225
Tony Majestro <tonymajestro@gmail.com>
2024-08-04 22:10:35 -0700

    Refactor opts parsing

--- lua/smart-scrolloff/init.lua
+++ lua/smart-scrolloff/init.lua
@@ -4,17 +4,15 @@ local DEFAULT_SCROLLOFF_PERCENTAGE = 0.2
 local OPTS_ERROR = "Error initializing smart-scrolloff.nvim: scrolloff_percentage must be a greater than or equal to 0."
 
 local get_scrolloff_percentage = function(opts)
-	local scrolloff_percentage = DEFAULT_SCROLLOFF_PERCENTAGE
-
-	if opts.scrolloff_percentage then
-		if type(opts.scrolloff_percentage) ~= "number" or opts.scrolloff_percentage < 0 then
-			error(OPTS_ERROR)
-		end
-
-		scrolloff_percentage = opts.scrolloff_percentage
+	if opts.scrolloff_percentage == nil then
+		return DEFAULT_SCROLLOFF_PERCENTAGE
 	end
 
-	return scrolloff_percentage
+	if type(opts.scrolloff_percentage) ~= "number" or opts.scrolloff_percentage < 0 then
+		error(OPTS_ERROR)
+	end
+
+	return opts.scrolloff_percentage
 end
 
 M.setup = function(opts)
